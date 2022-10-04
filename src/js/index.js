013e1b19fd0d5870aa2396e57f4dc18413b04227
Tonya-Shyva <toshash69@gmail.com>
2022-10-04 09:42:44 +0300

    added bootstrap spinner and worked with class LoadMoreBtn

--- src/js/index.js
+++ src/js/index.js
@@ -4,16 +4,19 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import photoMarkupTemplate from '../templates/photoCard.hbs';
 
+import LoadMoreBtn from './load-more-btn';
+
 const inputRef = document.querySelector('input[type="text"]');
 const btnSearch = document.querySelector('.btn-submit');
 const gallery = document.querySelector('.gallery');
-const btnLoadMore = document.querySelector('.load-more');
 const btnToTop = document.querySelector('.to-top');
+
+const loadMoreBtn = new LoadMoreBtn({ selector: '.load-more', hidden: true });
 
 const gallerySimpleLightbox = new SimpleLightbox('.gallery a');
 let pageNumber = 1;
 
-btnLoadMore.style.display = 'none';
+// btnLoadMore.style.display = 'none';
 btnToTop.style.display = 'none';
 
 btnSearch.addEventListener('click', onBtnSearchClick);
@@ -23,21 +26,27 @@ function onBtnSearchClick(e) {
   cleanGallery();
   const trimmedValue = inputRef.value.trim();
   if (trimmedValue !== '') {
+    loadMoreBtn.show();
+    loadMoreBtn.disable();
     fetchImages(trimmedValue, pageNumber).then(data => {
       //   console.log('onBtnSearchClick', data);
       if (data.totalHits === 0) {
+        loadMoreBtn.hide();
         Notiflix.Notify.failure(
           'Sorry, there are no images matching your search query. Please try again.'
         );
       } else {
         renderImageList(data.hits);
+
+        loadMoreBtn.disable();
         Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
         if (data.totalHits > 0 && data.totalHits < 40) {
+          loadMoreBtn.hide();
           Notiflix.Notify.failure(
             "We're sorry, but you've reached the end of search results."
           );
         } else {
-          btnLoadMore.style.display = 'block';
+          loadMoreBtn.enable();
         }
         gallerySimpleLightbox.refresh();
       }
@@ -45,25 +54,25 @@ function onBtnSearchClick(e) {
   }
 }
 
-btnLoadMore.addEventListener('click', onBtnLoadMoreClick);
+loadMoreBtn.refs.button.addEventListener('click', onBtnLoadMoreClick);
 
 function onBtnLoadMoreClick() {
   const trimmedValue = inputRef.value.trim();
   pageNumber += 1;
   btnToTop.style.display = 'inline-flex';
+  loadMoreBtn.disable();
   fetchImages(trimmedValue, pageNumber).then(data => {
+    renderImageList(data.hits);
     // console.log('onBtnLoadMoreClick', data);
     if (data.hits.length === 0 || data.hits.length < 40) {
-      renderImageList(data.hits);
       scrollByAfterLoadMore();
       Notiflix.Notify.failure(
         "We're sorry, but you've reached the end of search results."
       );
-      btnLoadMore.style.display = 'none';
-      //   console.log(btnLoadMore);
+      loadMoreBtn.hide();
     } else {
-      renderImageList(data.hits);
       scrollByAfterLoadMore();
+      loadMoreBtn.enable();
 
       gallerySimpleLightbox.refresh();
     }
@@ -73,44 +82,13 @@ function onBtnLoadMoreClick() {
 function renderImageList(images) {
   // ------з використанням шаблонізатора handlebars-----------------------
   gallery.insertAdjacentHTML('beforeend', photoMarkupTemplate(images));
-
-  // -------якщо без шаблонізатора----------------------------------------
-  //   const markup = images
-  //     .map(image => {
-  //       return photoMarkupTemplate(image);
-  //     })
-  //     .join('');
-  //   console.log(markup);
-  //   const markup = images
-  //     .map(image => {
-  //       console.log('img', image);
-  //       return `<div class="photo-card">
-  //            <a href="${image.largeImageURL}"><img class="photo" src="${image.webformatURL}" alt="${image.tags}" title="${image.tags}" loading="lazy" width="262" height="200"/></a>
-  //             <div class="info">
-  //                <p class="info-item">
-  //         <b>Likes</b> <span class="info-item-api"> ${image.likes} </span>
-  //     </p>
-  //                 <p class="info-item">
-  //                     <b>Views</b> <span class="info-item-api">${image.views}</span>
-  //                 </p>
-  //                 <p class="info-item">
-  //                     <b>Comments</b> <span class="info-item-api">${image.comments}</span>
-  //                 </p>
-  //                 <p class="info-item">
-  //                     <b>Downloads</b> <span class="info-item-api">${image.downloads}</span>
-  //                 </p>
-  //             </div>
-  //         </div>`;
-  //     })
-  //     .join('');
-  // gallery.insertAdjacentHTML('beforeend', markup);
 }
 
 function cleanGallery() {
   gallery.innerHTML = '';
   pageNumber = 1;
   btnToTop.style.display = 'none';
-  btnLoadMore.style.display = 'none';
+  loadMoreBtn.hide();
 }
 
 function scrollByAfterLoadMore() {
